032d601c54adcb02bfaaed30919af6f42a733fe2
sam <samuelchan@qq.com>
2019-06-22 22:14:10 +0800

    Added some sample codes

--- rest-poc-eureka/src/main/java/tech/comfortheart/gateway/MainController.java
+++ rest-poc-eureka/src/main/java/tech/comfortheart/gateway/MainController.java
@@ -1,8 +1,12 @@
 package tech.comfortheart.gateway;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
 
 @RestController
 public class MainController {
@@ -16,5 +20,10 @@ public class MainController {
     @GetMapping("/")
     public String hello() {
         return "Hello world at " + appName + ":" + port;
+    }
+
+    @GetMapping("/download")
+    public Mono<Resource> download() {
+        return Mono.just(new FileSystemResource("/Users/sam/Downloads/redis-5.0.5.tar"));
     }
 }
--- rest-spring-mvc/src/main/java/tech/comfortheart/gateway/MainController.java
+++ rest-spring-mvc/src/main/java/tech/comfortheart/gateway/MainController.java
@@ -2,7 +2,7 @@ package tech.comfortheart.gateway;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.codec.binary.Base64OutputStream;
 import org.springframework.http.MediaType;
 import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,18 +14,63 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 @RestController
 public class MainController {
 
 
-    String path = "/Users/sam/Downloads/redis-5.0.5.tar1";
+    String path = "/Users/sam/Downloads/redis-5.0.5.tar";
     @GetMapping("/download-one")
     public String downloadFileAsBase64One() throws IOException {
         byte[] bytes = Files.readAllBytes(new File(path).toPath());
 
         return Base64Utils.encodeToString(bytes);
+    }
+
+    @GetMapping("/download-three")
+    public void downloadFileAsBase64Three(HttpServletResponse response) throws IOException {
+        Files.copy(Paths.get(path), new Base64OutputStream(response.getOutputStream()));
+    }
+
+    @GetMapping("/download-four")
+    public void downloadFileAsBase64Four(HttpServletResponse response) throws IOException {
+        Files.copy(Paths.get(path), response.getOutputStream());
+    }
+
+
+    @GetMapping("/download-five")
+    public void downloadFileAsBase64Five(HttpServletResponse response) throws IOException {
+
+        try {
+            OutputStream out = response.getOutputStream();
+            response.setHeader("Content-Type", MediaType.APPLICATION_OCTET_STREAM.toString());
+            int bufLen = 4096;
+            byte[] buf = new byte[bufLen];
+            FileInputStream ins = new FileInputStream(path);
+            int len = ins.read(buf);
+            while (len > 0) {
+                if (len < bufLen) {
+                    out.write(Arrays.copyOf(buf, len));
+                } else {
+                    out.write(buf);
+                }
+                out.flush();
+                len = ins.read(buf);
+            }
+            out.close();
+        } catch (IOException e) {
+            ErrorResponse er = new ErrorResponse("E001", e.getMessage());
+            try {
+                response.setStatus(500);
+                OutputStream out = response.getOutputStream();
+                out.write(new ObjectMapper().writer().writeValueAsBytes(er));
+                out.close();
+            } catch (JsonProcessingException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     @GetMapping("/download-two")
